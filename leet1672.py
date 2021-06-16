9da9c6b789a9a26b57e9b5b1cfd6615565e4fafa
erdoganpak <erdoganpak@gmail.com>
2021-06-16 20:25:58 +0300

    Update leet1672.py

--- leet1672.py
+++ leet1672.py
@@ -1,1 +1,16 @@
 
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        richest = 0
+        current = 0;
+        for customer in accounts:
+            for value in customer:
+                current += value
+            if current > richest:
+                richest = current
+            current = 0
+        return richest
