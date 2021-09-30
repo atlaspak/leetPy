6370c8b07b4d9f46c022881b456a4356120f0c2d
erdoganpak <erdoganpak@gmail.com>
2021-09-30 09:59:50 +0200

    Update leet00009.py

--- leet00009.py
+++ leet00009.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        j = len(x) - 1
+        for num in x:
+            if num != x[j]:
+                return False
+            j -= 1
+            
+        return True
+        
