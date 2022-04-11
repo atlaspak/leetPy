c6f51e812dfb281a4b09c5d6241a41ce515a1c28
erdopak <erdoganpak@gmail.com>
2022-04-11 18:43:10 +0200

    Delete TODO 00004.py

--- TODO 00004.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        i1 = 0
-        i2 = 0
-        singlePoint = 0
-        doublePoint = 0
-        totalLen = (len(nums1) + len(nums2)) // 2
-        
-        while totalLen > 0:
